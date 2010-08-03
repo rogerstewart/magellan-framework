b1dea6a76b956a676546adc2b903538dce888e56
paul <paul@localhost>
2010-08-03 22:51:12 +1000

    Added SNK for signing all assemblies.

--- src/Magellan.Behaviors/Properties/AssemblyInfo.cs
+++ src/Magellan.Behaviors/Properties/AssemblyInfo.cs
@@ -2,9 +2,8 @@
 using System.Runtime.InteropServices;
 using System.Windows.Markup;
 using System.Runtime.CompilerServices;
-using System.Windows;
 
-[assembly: AssemblyTitle("Magellan Behaviors")]
+[assembly: AssemblyTitle("Magellan.Behaviors")]
 [assembly: AssemblyDescription("Magellan is a library that makes it easy to build WPF navigation applications")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Paul Stovell")]
@@ -14,8 +13,7 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: ThemeInfo(ResourceDictionaryLocation.SourceAssembly, ResourceDictionaryLocation.SourceAssembly)]
-
 [assembly: XmlnsDefinition("http://xamlforge.com/magellan", "Magellan.Behaviors")]
+[assembly: XmlnsPrefix("http://xamlforge.com/magellan", "magellan")]
 
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "Magellan.Behaviors")]
